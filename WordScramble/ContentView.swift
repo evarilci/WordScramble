5e886ebbecb8a0d7ae8bf69b516addeb2ba7aad0
Eymen <evarilci@gmail.com>
2022-08-11 22:14:21 +0300

    initial commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,112 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Eymen Varilci on 11.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var newWord = ""
+    @State private var rootWord = ""
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    var body: some View {
+        
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                    
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) {word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle.fill")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(alertMessage)
+            }
+        }
+        
+    }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {return}
+        
+        // more method comes here
+        guard isOriginal(word: answer) else {
+            wordAlert(title: "Word used already", message: "Think harder!")
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordAlert(title: "Word is not possible!", message: "You can't spell that from '\(rootWord)'!")
+            return
+        }
+        guard isReal(word: answer) else {
+            wordAlert(title: "Hmm", message: "Seems like you made it up...")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let url = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let contents = try? String(contentsOf: url) {
+                let letter = contents.components(separatedBy: "\n")
+                rootWord = letter.randomElement() ?? "Milkyway"
+                return
+            }
+        }
+        fatalError("could not load the start.txt from the bundle.")
+    }
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    func isReal(word: String) -> Bool  {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelword = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelword.location == NSNotFound
+    }
+    func wordAlert(title: String, message: String) {
+        alertTitle = title
+        alertMessage = message
+        showingAlert = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Eymen Varilci on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
